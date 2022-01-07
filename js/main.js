f9bdbb6cc85e8e64faae9342e0325a945cef9835
ISLOMIDDIN98 <najmiddinovislomiddin2gmail.com>
2022-01-07 23:07:17 +0900

    add

---
+++ js/main.js
@@ -0,0 +1,8 @@
+const Dollor = document.querySelector('.box-how-much');
+
+const Button = document.querySelector('.site-top-buttons');
+
+Button.addEventListener('click', function(){
+
+Dollor.textContent = '99';
+});
